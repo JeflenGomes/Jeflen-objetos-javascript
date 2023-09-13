6e538109bd2ac34f47fa23e96e8c023c57b302fd
JeflenGomes <jeflenpoteito@hotmail.com>
2023-09-13 20:19:01 -0300

    objetos-js

--- objeto.js
+++ objeto.js
@@ -1,7 +1,7 @@
 const listaCPFs = [1111111, 22222, 3333];
 
 const informacoesPessoa = ["nome", "Jeflen", "idade", 32, "CPF", "1111222333"];
-
+//
 console.log(informacoesPessoa[1]);
 
 const objetoPessoa = {
