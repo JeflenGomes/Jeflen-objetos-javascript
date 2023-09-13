ccc0742c037d405781cfea3e1f71dd28c89e5e71
JeflenGomes <jeflenpoteito@hotmail.com>
2023-09-13 20:39:27 -0300

    funcionouessaporra

--- objeto.js
+++ objeto.js
@@ -1,10 +1,10 @@
 const listaCPFs = [1111111, 22222, 3333];
 
 const informacoesPessoa = ["nome", "Jeflen", "idade", 32, "CPF", "1111222333"];
-//
+// jeflen gomes santos
 console.log(informacoesPessoa[1]);
 
 const objetoPessoa = {
   idade: 32,
-  nome: "Jos",
+  nome: "Jeflen",
 };
