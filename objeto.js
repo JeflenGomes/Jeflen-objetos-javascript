932b47948c1294c909f98cd9b00861be19a05f36
JeflenGomes <jeflenpoteito@hotmail.com>
2023-09-14 21:32:44 -0300

    att-objetos

--- objeto.js
+++ objeto.js
@@ -1,6 +1,6 @@
 const listaCPFs = [123456789, 123456789, 123456789];
 
-const informacoesPessoa = ["nome", "Jeflen", "idade", 30, "CPF", "1111222333"];
+const informacoesPessoa = ["nome", "Jeflen", "idade", 30, "CPF", "123456789"];
 // jeflen gomes santos
 console.log(informacoesPessoa[1]);
 
